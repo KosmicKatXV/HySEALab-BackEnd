4339d61ff6c47769ed374377362d6591ed4923c7
Pablo Tores Rodriguez <pablotoresrodriguez@gmail.com>
2025-05-12 18:55:28 +0200

    created models for labs, jobs and config management

--- auth-system-backend/job_dispatcher/models.py
+++ auth-system-backend/job_dispatcher/models.py
@@ -1,3 +1,24 @@
 from django.db import models
-
+from asb.models import CustomUser
+from k8s.models import Lab
 # Create your models here.
+
+
+class Config(models.Model):
+    name = models.TextField()
+    field_name = models.FileField(upload_to=None, max_length=254)
+    REQUIRED_FIELDS = ['user','lab','start_date']
+
+    def __str__(self):
+        return self.id
+
+class Job(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    lab = models.ForeignKey(Lab, on_delete=models.CASCADE)
+    config = models.ForeignKey(Config, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    REQUIRED_FIELDS = ['user','lab','start_date']
+
+    def __str__(self):
+        return self.id
--- auth-system-backend/k8s/models.py
+++ auth-system-backend/k8s/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from asb.models import CustomUser
 
 # Create your models here.
+
+class Lab(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    REQUIRED_FIELDS = ['user','start_date']
+
+    def __str__(self):
+        return self.id
