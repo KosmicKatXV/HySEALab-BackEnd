381163a286325547efabb91241f6d129d0b5835d
Pablo Tores Rodriguez <pablotoresrodriguez@gmail.com>
2025-12-11 10:36:35 +0100

    init improvement

--- auth-system-backend/init.sh
+++ auth-system-backend/init.sh
@@ -1,6 +1,9 @@
 rm -f db.sqlite3
+minikube delete -all
 
+minikube kubectl -- --namespace=hysealab delete all --all
 minikube kubectl -- --namespace=hysealab delete pvc --all
+minikube kubectl -- --namespace=hysealab delete pv --all
 minikube kubectl delete namespace hysealab
 minikube kubectl create namespace hysealab
 
